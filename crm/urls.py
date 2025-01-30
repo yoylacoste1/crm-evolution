28442806892ed0e05e8de734eda78c6f4ceb691a
yoylacoste1 <evolutiondish@gmail.com>
2025-01-29 20:23:35 -0500

    Update urls.py

--- crm/urls.py
+++ crm/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views  # Importar las vistas de la app
 
 urlpatterns = [
-    path('', views.login_view, name='login'),  # Cambié 'dashboard' por 'login'
+    path('', views.login_view, name='login'),  
     path('panel/', views.panel, name='panel_control'),
 ]
