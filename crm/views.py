0d359f18a94d260080819d77c26b79ad80756042
yoylacoste1 <evolutiondish@gmail.com>
2025-01-29 20:24:05 -0500

    Update views.py

--- crm/views.py
+++ crm/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render
 
 # Vista para Login
 def login_view(request):
-    return render(request, 'registration/login.html')  # Asegúrate que esté la ruta correcta de tu HTML
-
+    return render(request, 'registration/login.html')  
 # Vista para Panel de Control
 def panel(request):
-    return render(request, 'registration/panel_de_control.html')  # Asegúrate de tener la ruta correcta
+    return render(request, 'registration/panel_de_control.html')  
